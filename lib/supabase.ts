5a37e0aae59970d1687de45d47bfbd34e623f937
msmejoraprocesos <msmejoraprocesos@gmail.com>
2026-06-06 21:59:04 -0600

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,5 @@
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import type { Database } from '@/types/database'
+
+export const supabaseClient = () =>
+  createClientComponentClient<Database>()
